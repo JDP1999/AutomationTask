dc81bb6168ec3f55e7e9abe995d71e13fd536eab
Johannes Portzehl <johannes.portzehl@sogeti.com>
2025-04-28 14:57:31 +0200

    Create Methods in AutomationMethods

--- cypress/support/Pagebobjects/AutomationMethods.js
+++ cypress/support/Pagebobjects/AutomationMethods.js
@@ -23,20 +23,26 @@ class AutomationMethods{
         cy.get(element).realHover()
     }
     sendText(element,text){
-        //Check if first name exists
+        //Check if element exists
         cy.get(element).should('exist')
 
-        //Check if first name is visible
+        //Scroll element into view
+        cy.get(element).scrollIntoView()
+
+        //Check if element is visible
         cy.get(element).should('be.visible')
 
-        //Write random first name with faker
+        //Write random data with faker
         cy.get(element).type(text, {force:true})
     }
     slide(element){
-        //Check if slider exists
+        //Check if element exists
         cy.get(element).should('exist')
 
-        //Check if slider is visible
+        //Scroll element into view
+        cy.get(element).scrollIntoView()
+
+        //Check if element is visible
         cy.get(element).should('be.visible')
 
         //Slide to the left
